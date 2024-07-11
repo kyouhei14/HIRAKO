3302a3f0ad9d05d103850cdcc4d5b9f85ae886d7
Sunrwku!! <158478884+kyouhei14@users.noreply.github.com>
2024-07-11 00:23:14 -0700

    Update Footer.jsx

--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -1,6 +1,6 @@
 import Logo from "../ui/Logo";
 import Link from "next/link";
-import { FaGithub, FaDiscord, FaTwitter } from 'react-icons/fa';
+import { FaGithub, FaDiscord, FaFacebookF } from 'react-icons/fa';
 
 function Footer() {
   return (
@@ -24,17 +24,17 @@ function Footer() {
       <footer className="pb-10 px-5 md:px-12 lg:px-16 xl:px-20 2xl:px-24">
         <div className="pt-2 flex justify-between border-t-[2px] border-primary w-full">
           <p className="z-50 dark:text-primary-light text-primary text-sm">
-            Made with ❤️ by <Link title="Jeydin21" className="hover:text-blue-400 dark:text-secondary text-primary underline transition-all" rel="noreferrer" target="_blank" href="https://j21.dev">Jeydin21</Link>
+            Made with ❤️ by <Link title="Jeydin21" className="hover:text-blue-400 dark:text-secondary text-primary underline transition-all" rel="noreferrer" target="_blank" href="rajveer.is-a.dev">rajveer</Link>
           </p>
           <div className="z-50 flex flex-row items-center space-x-3">
-            <Link title="Community Discord Server" href="https://discord.com/invite/JQsvHC4JUH" target="_blank" rel="noreferrer" className="transition-all hover:scale-125">
+            <Link title="Community Discord Server" href="https://discord.com/invite/portmafia" target="_blank" rel="noreferrer" className="transition-all hover:scale-125">
               <FaDiscord size={25} color="grey " />
             </Link>
             <Link title="Hirako Source Code" href="https://github.com/kyohei14" target="_blank" rel="noreferrer" className="transition-all hover:scale-125">
               <FaGithub size={25} color="grey" />
             </Link>
-            <Link title="Jeydin21's Twitter" href="https://x.com/Jeydin21" target="_blank" rel="noreferrer" className="transition-all hover:scale-125">
-              <FaTwitter size={25} color="grey" />
+            <Link title="Hirako's fb" href="https://facebook.com/hirakoisdead" target="_blank" rel="noreferrer" className="transition-all hover:scale-125">
+              <FaFacebookF size={25} color="grey" />
             </Link>
           </div>
         </div>
