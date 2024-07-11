5126620c812828a0b26d704f9e07bd3adcb2a300
Sunrwku!! <158478884+kyouhei14@users.noreply.github.com>
2024-07-11 00:43:52 -0700

    Update Footer.jsx

--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -1,6 +1,6 @@
 import Logo from "../ui/Logo";
 import Link from "next/link";
-import { FaGithub, FaInstagram, FaFacebookF } from 'react-icons/fa';
+import { SiValorant, FaInstagram, FaFacebookF } from 'react-icons/fa';
 
 function Footer() {
   return (
@@ -24,14 +24,14 @@ function Footer() {
       <footer className="pb-10 px-5 md:px-12 lg:px-16 xl:px-20 2xl:px-24">
         <div className="pt-2 flex justify-between border-t-[2px] border-primary w-full">
           <p className="z-50 dark:text-primary-light text-primary text-sm">
-            Made with ❤️ by <Link title="Hirako" className="hover:text-blue-400 dark:text-secondary text-primary underline transition-all" rel="noreferrer" target="_blank" href="rajveer.is-a.dev">rajveer</Link>
+            Made with ❤️ by <Link title="Hirako" className="hover:text-blue-400 dark:text-secondary text-primary underline transition-all" rel="noreferrer" target="_blank" href="anime">rajveer</Link>
           </p>
           <div className="z-50 flex flex-row items-center space-x-3">
             <Link title="Hirako's ig" href="https://instagram.com/hirakoisnumb" target="_blank" rel="noreferrer" className="transition-all hover:scale-125">
               <FaInstagram size={25} color="grey " />
             </Link>
-            <Link title="Hirako Source Code" href="https://github.com/kyohei14" target="_blank" rel="noreferrer" className="transition-all hover:scale-125">
-              <FaGithub size={25} color="grey" />
+            <Link title="rajveer's portfolio" href="rajveer.is-a.dev" target="_blank" rel="noreferrer" className="transition-all hover:scale-125">
+              <SiValorant size={25} color="grey" />
             </Link>
             <Link title="Hirako's fb" href="https://facebook.com/hirakoisdead" target="_blank" rel="noreferrer" className="transition-all hover:scale-125">
               <FaFacebookF size={25} color="grey" />
